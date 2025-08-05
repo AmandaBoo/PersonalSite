5573dae23bd5baec1390c1659e0e7f8f4bb25a72
Amanda <amandabui1590@gmail.com>
2025-08-05 17:33:17 -0500

    remove instagram link

--- src/pages/Nav/components/ExternalSocialLink.js
+++ src/pages/Nav/components/ExternalSocialLink.js
@@ -1,6 +1,5 @@
 import {SvgLinkIcon} from "./SvgLinkIcon";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import React from "react";
 
@@ -9,9 +8,6 @@ export const ExternalSocialLinks = () => {
         <div className={'flex justify-center'}>
             <SvgLinkIcon url={'https://github.com/AmandaBoo/'}>
                 <GitHubIcon className={'text-brown hover:'} fontSize={'large'}/>
-            </SvgLinkIcon>
-            <SvgLinkIcon url={'https://www.instagram.com/amanda_boo__/'}>
-                <InstagramIcon className={'text-brown'} fontSize={'large'}/>
             </SvgLinkIcon>
             <SvgLinkIcon url={'https://www.linkedin.com/in/amandabui-dev/'}>
                 <LinkedInIcon className={'text-brown'} fontSize={'large'}/>
